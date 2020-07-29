f64014d663492daadfc2633bfc76f02ff947d997
Chase Gaddis <cgaddis36@gmail.com>
2020-07-29 10:00:14 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'json'
 
 get '/zipradius' do
   conn = Faraday.new("https://www.zipwise.com")
-  response = conn.get("/webservices/radius.php?key=#{ENV['ZIPWISE_KEY']}&zip=#{params[:zip]}&radius=15&format=json")
+  response = conn.get("/webservices/radius.php?key=#{ENV['ZIPWISE_KEY']}&zip=#{params[:zip]}&radius=#{params[:radius]}&format=json")
   zipcodes = JSON.parse(response.body, symbolize_names: true)
   
   zipcodes[:results].map{|result| result[:zip]}
